c47c802e6134caaed83e69b4fff5e565ddea0b3d
feminite-diva <kpysanenko@huspi.com>
2023-10-15 17:42:32 +0300

    Form in modal

--- product list - modal/src/components/modal.jsx
+++ product list - modal/src/components/modal.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, {useRef} from "react";
 
 export const Modal = ({ openModal, setOpenModal }) => {
+    const checkboxRef = useRef(null);
+    const categoryRef = useRef(null);
+    const priceRef = useRef(null);
+    const nameRef = useRef(null);
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        alert(`Name: ${categoryRef.current.value}, Price: ${priceRef.current.value}, stocked: ${checkboxRef.current.checked}, Name: ${nameRef.current.value}`);
+    }
+
     return <>
         <div className="modal-overlay">
             <div className="modal-container">
@@ -16,17 +26,47 @@ export const Modal = ({ openModal, setOpenModal }) => {
                     </button>
                 </div>
                 <div className="modal-content">
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            type="text"
+                            name="category"
+                            placeholder="Category"
+                            ref={categoryRef}
+                        />
+                        <input
+                            type="text"
+                            name="price"
+                            placeholder="Price"
+                            ref={priceRef}
+                        />
+                        <div>
+                            <label>Stocked</label>
+                            <input
+                                type="checkbox"
+                                name="stocked"
+                                ref={checkboxRef}
+                            />
+                        </div>
+                        <input
+                            type="text"
+                            name="name"
+                            placeholder="Name"
+                            ref={nameRef}
+                        />
 
-                </div>
-                <div className="modal-footer">
-                    <button>Submit</button>
-                    <button
-                        onClick={()=>{
-                            setOpenModal(false);
-                        }}
-                    >
-                        Cancel
-                    </button>
+                        <div className="modal-footer">
+                            <button type="submit">
+                                    Add to list
+                            </button>
+                            <button type="button"
+                                onClick={()=>{
+                                    setOpenModal(false);
+                                }}
+                            >
+                                Cancel
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
